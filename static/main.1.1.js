254b83b8bf0409a8f0a3199a5f9e54761080c9e4
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2021-08-17 21:45:21 +0300

    update left hot bar & add login form

--- static/main.1.1.js
+++ static/main.1.1.js
@@ -4,7 +4,7 @@ var current_page = "page_home";
 //var closed_page = "";
 
 for (let i = 0; i < page_names.length; i++){
-  document.getElementById(page_names[i]).style.left = -1900;
+  document.getElementById(page_names[i]).style.left = 2000;//-1900;
 }
 
 function closeModal(modalId) {
@@ -25,9 +25,19 @@ function openModal(modalId) {
 
 open_page("page_home");
 
+/*document.getElementById("left_hotbar").addEventListener("mouseover", function( event ) {
+  // highlight the mouseover target
+  console.log('open')
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    console.log('close')
+  }, 500);
+}, false);*/
+
 function left_hot_bar() {
   if (!document.getElementById("hmt").checked){
-    document.getElementById(current_page).style.transition = "0.5s all";
+    document.getElementById(current_page).style.transition = "0.2s all";
     document.getElementById(current_page).style.left = 100;
   } else {
     document.getElementById(current_page).style.transition = "0.5s all";
@@ -38,9 +48,9 @@ function left_hot_bar() {
 function open_page(page_name, animation=false){ //переключение страниц
   var opened = false;
 
-  document.getElementById("hmt").checked = false;
+  //document.getElementById("hmt").checked = false;
 
-  document.getElementById(current_page).style.left = -1900;
+  document.getElementById(current_page).style.left = 2000;//1900;
   document.getElementById(current_page).style.transition = "0.2s all";
 
   for (let i = 0; i < page_names.length; i++){
@@ -64,7 +74,18 @@ function open_page(page_name, animation=false){ //переключение ст�
     document.getElementById(page_name).style.transition = "0.2s all";
     document.getElementById(page_name).style.left = 100;
     current_page = page_name;
+    left_hot_bar();
   }
+}
+
+var left_hotbar = document.getElementById('left_hotbar');
+left_hotbar.onmouseover = function() {
+  document.getElementById("hmt").checked = true;
+  left_hot_bar();
+}
+left_hotbar.onmouseout = function(e) {
+  document.getElementById("hmt").checked = false;
+  left_hot_bar();
 }
 
 function close_main_loading() {
@@ -73,3 +94,10 @@ function close_main_loading() {
 document.getElementById("loading_background").style.opacity = 0;
 var element = document.getElementById("loading_background");
 element.addEventListener("transitionend", close_main_loading, false);
+
+function resize() {
+  document.body.style.zoom = document.body.clientWidth / (1920);
+}
+
+resize();
+window.addEventListener('resize', resize, true);
