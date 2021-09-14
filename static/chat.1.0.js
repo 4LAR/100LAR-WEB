0b7e6cb40f99823031486ab2776a558f2655ae53
100LAR [Stolar Studio] <nikitstolarov@gmail.com>
2021-09-14 15:32:34 +0300

    Обновление чатов

---
+++ static/chat.1.0.js
@@ -0,0 +1,32 @@
+function append_to_chat_messages_list(name, message, time) {
+	var ul = document.getElementById("chat_messages_list");
+	var li = document.createElement("li");
+
+	li.innerHTML = `
+	<div class="message_container">
+	  <image class="icon" width="60" height="60" src="static/img/profile.svg">
+	  <h2 class="message_username_class">` + name + `</h1>
+	  <h3 class="message_text_class">` + message + `</h3>
+	  <p class="message_time_class  ">` + time + `</p>
+	</div>
+	`;
+
+	li.setAttribute("id", "element4");
+	ul.appendChild(li);
+}
+
+function append_to_chats(name, type) {
+	var ul = document.getElementById("chats_list");
+	var li = document.createElement("li");
+
+	li.innerHTML = `
+	<div class="chats_names_list">
+	  <image style="float: left" class="icon" width="60" height="60" src="static/img/profile.svg">
+	  <h2>` + name + `</h1>
+	  <h3>` + type + `</h3>
+	</div>
+	`;
+
+	li.setAttribute("id", "element4");
+	ul.appendChild(li);
+}
